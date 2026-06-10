6119d968af0760e4475c59854fe32b58b3e0aa0b
Naman Chauhan <namansinghchauhan6376@gmail.com>
2026-06-10 19:28:28 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ let project1 = document.querySelector(".container1")
 project1.addEventListener("click", function(){
     window.location.href = "./project1/index.html"
 })
+
+let navbarTitle = document.querySelector(".title")
+let navbarName = document.querySelector(".name")
+let navbarConnect = document.querySelector(".connect")
+let hamburger = document.querySelector(".hamburger")
+
+hamburger.addEventListener("click", function(){
+    navbarTitle.classList.toggle("show-menu")
+    navbarName.classList.toggle("show-menu")
+    navbarConnect.classList.toggle("show-menu")
+})

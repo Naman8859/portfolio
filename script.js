6eae748fec883f27d137a262254e0dbc7a62ef99
Naman Chauhan <namansinghchauhan6376@gmail.com>
2026-06-10 19:54:52 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ hamburger.addEventListener("click", function(){
     navbarName.classList.toggle("show-menu")
     navbarConnect.classList.toggle("show-menu")
 })
+
+let instagram = document.querySelector(".instagram")
+let linkedin = document.querySelector(".linkedin")
+let github = document.querySelector(".github")
+instagram.addEventListener("click", function(){
+    window.open("https://www.instagram.com/am_thakurnaman_00118?igsh=c3VzcHl3N3A3d2th&utm_source=qr")
+})
+
+linkedin.addEventListener("click", function(){
+    window.open("https://www.linkedin.com/in/naman-singh-chauhan-9702461b2/")
+})
+
+github.addEventListener("click", function(){
+    window.open("https://github.com/Naman8859")
+})
